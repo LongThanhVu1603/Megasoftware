381401954116bf9f7a60f97e4a150b6a67102523
Long Thanh Vu <Longthanhvu1603hn2@gmail.com>
2021-05-12 12:26:45 +0700

    Pull lần 1

---
+++ app.js
@@ -0,0 +1,39 @@
+// Button onclick
+
+function product()
+{
+    window.location.replace('./product.html');
+}
+
+function service()
+{
+    window.location.replace('./service.html');
+}
+
+var body = document.body,
+    html = document.documentElement;
+
+    var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+// Back To Top Button
+if ($('#back-to-top').length) {
+    
+    var scrollTrigger = height - 300, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop() + $(window).height();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+}
